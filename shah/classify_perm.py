ca0a83e0712382005ce21a87f65030ed5e7ada10
Md Shahrear Iqbal <shahrear.iqbal@gmail.com>
2017-06-11 11:22:29 -0400

    last from office sec mode paper

--- shah/classify_perm.py
+++ shah/classify_perm.py
@@ -2,6 +2,8 @@
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.naive_bayes import GaussianNB
 from sklearn import svm
+from sklearn import tree
+from sklearn import neighbors
 from sklearn.metrics import accuracy_score, f1_score, precision_score, recall_score, classification_report, confusion_matrix
 # Load pandas
 import pandas as pd
@@ -15,67 +17,60 @@ train = pd.read_csv('/home/shahrear/SHAH_PROJECTS/google-play-scraper/shah/train
 test = pd.read_csv('/home/shahrear/SHAH_PROJECTS/google-play-scraper/shah/test_data.csv')
 
 features = train.columns[:192]
-
-clf = RandomForestClassifier(n_jobs=-1)
-
-
-
-clf.fit(train[features], train['class'])
-
-preds = clf.predict(test[features])
-
-print list(zip(train[features], clf.feature_importances_))
-
-print pd.crosstab(test['class'], preds, rownames=['Actual'], colnames=['Predicted'])
-
-print precision_score(test['class'], preds, average='macro')
-
-print accuracy_score(test['class'], preds)
-
-print f1_score(test['class'], preds, average='macro')
-
-print recall_score(test['class'], preds, average='macro')
-
-print confusion_matrix(test['class'], preds)
-
-print classification_report(test['class'], preds)
-
+#
+# #gaussian
 # clf = GaussianNB()
 #
 # clf.fit(train[features], train['class'])
 #
 # preds = clf.predict(test[features])
 #
-# print pd.crosstab(test['class'], preds, rownames=['Actual'], colnames=['Predicted'])
-#
-# print precision_score(test['class'], preds, average='macro')
-#
-# print accuracy_score(test['class'], preds)
-#
-# print f1_score(test['class'], preds, average='macro')
-#
-# print recall_score(test['class'], preds, average='macro')
-#
 # print confusion_matrix(test['class'], preds)
 #
 # print classification_report(test['class'], preds)
 #
+# #svm
 # clf = svm.SVC()
 #
 # clf.fit(train[features], train['class'])
 #
 # preds = clf.predict(test[features])
 #
-# print pd.crosstab(test['class'], preds, rownames=['Actual'], colnames=['Predicted'])
+# print confusion_matrix(test['class'], preds)
 #
-# print precision_score(test['class'], preds, average='macro')
+# print classification_report(test['class'], preds)
 #
-# print accuracy_score(test['class'], preds)
+# #DecisionTree
+# clf = tree.DecisionTreeClassifier()
 #
-# print f1_score(test['class'], preds, average='macro')
+# clf.fit(train[features], train['class'])
 #
-# print recall_score(test['class'], preds, average='macro')
+# preds = clf.predict(test[features])
 #
 # print confusion_matrix(test['class'], preds)
 #
 # print classification_report(test['class'], preds)
+#
+# #kNN
+# clf = neighbors.KNeighborsClassifier(15, 'uniform')
+#
+# clf.fit(train[features], train['class'])
+#
+# preds = clf.predict(test[features])
+#
+# print confusion_matrix(test['class'], preds)
+#
+# print classification_report(test['class'], preds)
+
+#RandomForest
+clf = RandomForestClassifier(n_jobs=-1)
+
+clf.fit(train[features], train['class'])
+
+preds = clf.predict(test[features])
+
+print confusion_matrix(test['class'], preds)
+
+print classification_report(test['class'], preds)
+
+
